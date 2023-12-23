83cd19cd1d0b53ae7ee04b8173989e3518aca075
Saim Khan <93315183+KSAIM09@users.noreply.github.com>
2023-12-23 22:03:26 +0000

    some new changes

--- countries-searchx-app/src/App.js
+++ countries-searchx-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import './App.css';
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [search, setSearch] = useState("");
@@ -24,29 +24,41 @@ const App = () => {
       className="App"
       style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
     >
-      <input
-        type="text"
-        placeholder="Search..."
-        value={search}
-        onChange={handleSearchChange}
-      />
+      <nav
+        className="nav"
+        style={{ boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)", padding: "10px", width: "100%", marginBottom: "20px" }}
+      >
+        <input
+          type="text"
+          placeholder="Search Country"
+          value={search}
+          onChange={handleSearchChange}
+          style={{
+            width: "60%",
+            maxWidth: "100%",
+            padding: "8px",
+            marginTop: "30px",
+          }}
+        />
+      </nav>
       <div
         className="card-grid"
         style={{
           display: "grid",
           gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))",
           gridGap: "10px",
-          width: "100%",
+          width: "80%",
+          margin: "0px 40px"
         }}
       >
         {filteredCountries.map((country) => (
           <div
             className="card"
-            key={country.alpha3Code}
+            key={country.cca2}
             style={{
               border: "1px solid #ccc",
               padding: "20px",
-              boxShadow: "2px 2px 6px #ccc",
+              
               borderRadius: "5px",
               textAlign: "center",
             }}
